474c000412f3920e4c715fc958c0d5c230cee0e6
Ghisrich <ghislainfk@gmail.com>
2026-05-06 19:32:28 +0100

    Mise à jour du site

--- js/products.js
+++ js/products.js
@@ -1,17 +1,17 @@
 const products = [
     {
         id: 1,
-        name: "Cuisine moderne",
+        name: "Cuisine sur mesure",
         category: "cuisine",
-        price: 600000,
-        description: "Cuisine élégante sur mesure",
+        price: 0,
+        description: "Cuisine sur mesure, Prix sur dimensionnement et au choix",
         image: "images/00.jpeg"
     },
     {
         id: 2,
         name: "Placard formalisée",
         category: "placard",
-        price: 250000,
+        price: 300000,
         description: "Simple couleur au choix, Dimension au choix",
         image: "images/01.jpeg"
     },
@@ -19,7 +19,7 @@ const products = [
         id: 3,
         name: "Placard formalisée",
         category: "placard",
-        price: 200000,
+        price: 220000,
         description: "Simple couleur au choix, Dimension au choix",
         image: "images/02.jpeg"
     },
@@ -27,29 +27,29 @@ const products = [
         id: 4,
         name: "Placard formalisée",
         category: "placard",
-        price: 320000,
+        price: 400000,
         description: "Simple couleur au choix, Dimension au choix",
         image: "images/03.jpeg"
     },
     {
         id: 5,
-        name: "Cuisine moderne",
+        name: "Cuisine sur mesure",
         category: "cuisine",
-        price: 450000,
-        description: "Cuisine élégante sur mesure",
+        price: 0,
+        description: "Cuisine sur mesure, Prix sur dimensionnement et au choix",
         image: "images/04.jpeg"
     },
     {
         id: 6,
-        name: "Placard formalisée",
+        name: "Meuble Tv avec plaque formalisée",
         category: "placard",
-        price: 150000,
+        price: 300000,
         description: "Simple couleur au choix, Dimension au choix",
         image: "images/05.jpeg"
     },
     {
         id: 7,
-        name: "Placard formalisée",
+        name: "Meble TV avec plaque formalisée, tasseaux applique et rangements 5 étagère  ",
         category: "placard",
         price: 400000,
         description: "Simple couleur au choix, Dimension au choix",
@@ -57,7 +57,7 @@ const products = [
     },
     {
         id: 8,
-        name: "Placard formalisée",
+        name: "Meuble Tv",
         category: "placard",
         price: 350000,
         description: "Simple couleur au choix, Dimension au choix",
@@ -65,26 +65,26 @@ const products = [
     },
     {
         id: 9,
-        name: "Cuisine moderne",
+        name: "Cuisine sur mesure",
         category: "cuisine",
-        price: 380000,
-        description: "Cuisine élégante sur mesure",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
         image: "images/09.jpeg"
     },
     {
         id: 10,
-        name: "Cuisine moderne",
+        name: "Cuisine sur mesure",
         category: "cuisine",
-        price: 300000,
-        description: "Cuisine élégante sur mesure",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
         image: "images/10.jpeg"
     },
     {
         id: 11,
-        name: "Cuisine moderne",
+        name: "Cuisine sur mesure",
         category: "cuisine",
-        price: 500000,
-        description: "Cuisine élégante sur mesure",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
         image: "images/11.jpeg"
     },
     {
@@ -115,9 +115,9 @@ const products = [
         id: 15,
         name: "Placard formalisée",
         category: "placard",
-        price: 200000,
+        price: 0,
         description: "Simple couleur au choix, Dimension au choix",
-        image: "images/IMG-20260121-WA0002.jpeg"
+        image: "images/137.jpg"
     },
     {
        id: 16,
@@ -135,4 +135,293 @@ const products = [
         description: "Simple couleur au choix, Dimension au choix",
         image: "images/31.jpeg"
     },
+    {
+       id: 18,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/100.jpeg"
+    },
+    {
+       id: 19,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/101.jpeg"
+    },
+    {
+       id: 20,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/102.jpeg"
+    },
+    {
+       id: 21,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/103.jpeg"
+    },
+    {
+       id: 22,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/104.jpeg"
+    },
+    {
+       id: 23,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/105.jpeg"
+    },
+    {
+       id: 24,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/106.jpeg"
+    },
+     {
+       id: 25,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/107.jpeg"
+    },
+     {
+       id: 26,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/108.jpeg"
+    },
+     {
+       id: 27,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/109.jpeg"
+    },
+     {
+       id: 28,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/110.jpeg"
+    },
+     {
+       id: 29,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/111.jpeg"
+    },
+     {
+       id: 30,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/112.jpeg"
+    },
+     {
+       id: 31,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/113.jpeg"
+    },
+     {
+       id: 32,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/114.jpeg"
+    },
+     {
+       id: 33,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/115.jpeg"
+    },
+     {
+       id: 34,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/116.jpeg"
+    },
+     {
+       id: 35,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/117.jpeg"
+    },
+     {
+       id: 36,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/118.jpeg"
+    },
+     {
+       id: 37,
+        name: "Placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/119.jpeg"
+    },
+     {
+       id: 38,
+        name: "Cuisine moderne",
+        category: "cuisine",
+        price: 0,
+        description: "Cuisine élégante sur mesure",
+        image: "images/120.jpeg"
+    },
+     {
+       id: 39,
+        name: "Cuisine sur mesure",
+        category: "placard",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/121.jpeg"
+    },
+    {
+        id: 40,
+        name: "Placard formalisée",
+        category: "plaqcard",
+        price: 0,
+        description: "Simple couleur au choix, Dimension au choix",
+        image: "images/122.jpeg"
+    },
+    {
+        id: 41,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/123.jpeg"
+    },
+     {
+        id: 42,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/124.jpeg"
+    },
+    {
+        id: 43,
+        name: "Meuble TV",
+        category: "placard",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/128.jpeg"
+    },
+     {
+        id: 44,
+        name: "placard formalisée",
+        category: "placard",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/126.jpeg"
+    },
+     {
+        id: 45,
+        name: "Meuble TV",
+        category: "placard",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/127.jpeg"
+    },
+     {
+        id: 46,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/129.jpeg"
+    },
+     {
+        id: 47,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/130.jpeg"
+    },
+     {
+        id: 48,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/131.jpeg"
+    },
+     {
+        id: 49,
+        name: "Cuisine sur mesure",
+        category: "cuisine",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/132.jpeg"
+    },
+     {
+        id: 50,
+        name: "Dressing sur mesure",
+        category: "dressing",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/133.jpeg"
+    },
+     {
+        id: 51,
+        name: "Dressing sur mesure",
+        category: "dressing",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/135.jpeg"
+    },
+     {
+        id: 52,
+        name: "Dressing sur mesure",
+        category: "dressing",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/136.jpeg"
+    },
+     {
+        id: 53,
+        name: "Meuble TV",
+        category: "placard",
+        price: 0,
+        description: "Prix sur dimensionnement et au choix",
+        image: "images/125.jpeg"
+    },
+    
 ];
